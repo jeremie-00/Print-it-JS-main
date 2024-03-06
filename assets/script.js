e408bc601349d4bb004c73117406ec166f064eb5
jeremie <jeremie.herault@orange.fr>
2024-03-06 08:42:52 +0100

    defilement infini sur le carrousel

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,19 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
@@ -39,31 +39,27 @@ const updateSlide = (index) => {
 }
 
 function changeSlide(direction) {
-	
-   if (direction === 'left') {
-		currentSlide = currentSlide + -1
-   }else{
-		currentSlide = currentSlide + +1
-   }
-	
+	console.log(currentSlide)
+    currentSlide = (currentSlide + direction + slides.length) % slides.length
+	console.log(currentSlide)
     updateSlide(currentSlide)
 }
 
 for (let i = 0; i < slides.length; i++) {
     const dot = document.createElement('span')
     dot.classList.add('dot')
-	if (i === 0) {
-		dot.classList.add('dot_selected')
-	}
+    updateSlide(currentSlide)
     dots.appendChild(dot)
 }
 
 arrows.forEach(arrow => {
 	arrow.addEventListener('click', () => {
 		if (arrow.classList[1] === 'arrow_left') {
-			changeSlide('left')	
+			changeSlide(-1)		
 		} else {
-			changeSlide('right')		
+			changeSlide(+1)		
 		}
 	})
 })
+
+
